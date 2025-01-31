2ffc4e9abee304307a5fd665653dbac944c36d1b
Riiskager <emil.axel.riis@gmail.com>
2025-01-31 12:33:35 +0100

    alt

--- Video/vjs/video.js
+++ Video/vjs/video.js
@@ -9,8 +9,16 @@ function playVid() {
     x.pause(); 
   } 
 
-
+var l = document.getElementById('enya').autoplay;
+l.autoplay = true;
 
   document.getElementById('faxe').addEventListener('ended', function() {
     document.getElementById('faxe1').play();
   });
+
+  var d = document.getElementById('dåse')
+
+function knaps(){
+dåse.play
+
+}
